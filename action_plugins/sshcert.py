54549c5f007b47ad52ea9cf77614f0b7a9b06dae
Bastian Blank <waldi@debian.org>
2016-01-24 17:52:12 +0100

    Read key and certificate

--- action_plugins/sshcert.py
+++ action_plugins/sshcert.py
@@ -4,8 +4,11 @@
 
 
 import base64
+import codecs
+import collections
 import os
 import shutil
+import struct
 import subprocess
 import tempfile
 
@@ -51,6 +54,8 @@ class ActionModule(ActionBase):
             return result
 
         # XXX: Check existing cert
+        pubkey_data = SshFile.read(pubkey_content)
+        cert_data = SshFile.read(cert_content)
 
         tmp_local = tempfile.mkdtemp()
         try:
@@ -92,3 +97,103 @@ class ActionModule(ActionBase):
 
         finally:
             shutil.rmtree(tmp_local)
+
+
+class SshWirestring(object):
+    __slots__ = '__s',
+
+    def __init__(self, s):
+        self.__s = memoryview(s)
+
+    def read_string(self):
+        s = self.__s
+        l = struct.unpack_from('!L', s)[0]
+        self.__s = s[4+l:]
+        return s[4:4+l]
+
+    def read_uint32(self):
+        s = self.__s
+        self.__s = s[4:]
+        return struct.unpack_from('!L', s)[0]
+
+    def read_uint64(self):
+        s = self.__s
+        self.__s = s[8:]
+        return struct.unpack_from('!Q', s)[0]
+
+
+class SshKeyRsa(collections.namedtuple('SshKeyRsa', ('e', 'n'))):
+    __slots__ = ()
+
+    def __new__(cls, key):
+        return super(SshKeyRsa, cls).__new__(cls,
+                key.read_string(),
+                key.read_string())
+
+
+class SshKeyDsa(collections.namedtuple('SshKeyRsa', ('p', 'q', 'g', 'y'))):
+    __slots__ = ()
+
+    def __new__(cls, key):
+        return super(SshKeyDsa, cls).__new__(cls,
+                key.read_string(),
+                key.read_string(),
+                key.read_string(),
+                key.read_string())
+
+
+class SshKeyEcdsa(collections.namedtuple('SshKeyRsa', ('curve', 'pk'))):
+    __slots__ = ()
+
+    def __new__(cls, key):
+        return super(SshKeyEcdsa, cls).__new__(cls,
+                key.read_string(),
+                key.read_string())
+
+
+class SshKeyEd25519(collections.namedtuple('SshKeyRsa', ('pk', ))):
+    __slots__ = ()
+
+    def __new__(cls, key):
+        return super(SshKeyEd25519, cls).__new__(cls,
+                key.read_string())
+
+
+class SshCert(collections.namedtuple('SshCert', ('key', 'princs', 'validafter', 'validbefore'))):
+    __slots__ = ()
+
+    def __new__(cls, cert, key_type):
+        nonce = cert.read_string()
+        key = key_type(cert)
+        serial = cert.read_uint64()
+        type = cert.read_uint32()
+        keyid = cert.read_string().tobytes().decode('utf-8')
+        princs = cert.read_string().tobytes().decode('utf-8').split(',')
+        validafter = cert.read_uint64()
+        validbefore = cert.read_uint64()
+        return super(SshCert, cls).__new__(cls, key, princs, validafter, validbefore)
+
+
+class SshFile(object):
+    types = {
+        b'ssh-rsa': SshKeyRsa,
+        b'ssh-dss': SshKeyDsa,
+        b'ecdsa-sha2-nistp256': SshKeyEcdsa,
+        b'ecdsa-sha2-nistp384': SshKeyEcdsa,
+        b'ecdsa-sha2-nistp521': SshKeyEcdsa,
+        b'ssh-ed25519': SshKeyEd25519,
+    }
+
+    @classmethod
+    def read(cls, i):
+        i = i.split(' ', 2)
+        i_type = i[0]
+        i_data = SshWirestring(codecs.decode(i[1], 'base64'))
+        i_datatype = i_data.read_string()
+        if i_type != i_datatype:
+            raise RuntimeError
+
+        if i_type.endswith('-cert-v01@openssh.com'):
+            return SshCert(i_data, cls.types[i_type[0:-21]])
+        else:
+            return cls.types[i_type](i_data)
